556ff7856dbe051b885151e3ebe9415d1eb6adce
Daniel Freedman <38354462+MetalcoreLoli@users.noreply.github.com>
2019-05-12 14:10:57 +0300

    fix main loop

--- Actors.fs
+++ Actors.fs
@@ -33,45 +33,48 @@ open System
                     member val MaxHp = 100.0f with get, set
                     member val IsLive = true with get, set 
                     member val Sprite:Sprite = slimeSprite with get, set
-            member self.Collision(dir:int) = 
+            member self.Collision() = 
                                               for i = (int)((self :> IActor).Y / 32.0f) to (int)(( (self :> IActor).Y + 30.0f) / 32.0f) do 
                                                     for j = (int)((self :> IActor).X / 32.0f) to (int)(( (self :> IActor).X + 40.0f) / 32.0f) do 
                                                                                                                                            if Core.tileMap.[i].[j] = '#' then 
-                                                                                                                                                                         if dir = 0 then 
-                                                                                                                                                                                    if (self :> IActor).DX > 0.0f then (self :> IActor).X <- (float32)j * 32.0f - 40.5f
-                                                                                                                                                                                    if (self :> IActor).DX < 0.0f then (self :> IActor).X <- (float32)j * 32.0f + 40.0f
-                                                                                                                                                                         if dir = 1 then
-                                                                                                                                                                                     if (self :> IActor).DY > 0.0f then 
-                                                                                                                                                                                         (self :> IActor).Y <- (float32)i * 32.0f - 30.5f
-                                                                                                                                                                                         (self :> IActor).DY <- 0.0f
-                                                                                                                                                                                         self.slimeOnGround <- true
-                                                                                                                                                                                     //potolok
-                                                                                                                                                                                     if (self :> IActor).DY < 0.0f then 
-                                                                                                                                                                                         (self :> IActor).Y <- (float32)i * 32.0f + 32.5f
-                                                                                                                                                                                         (self :> IActor).DY <- 0.0f
+                                                                                                                                                                    
+                                                                                                                                                                            if (self :> IActor).DX > 0.0f then 
+                                                                                                                                                                                                        (self :> IActor).X <- (float32)j * 32.0f - 40.5f
+                                                                                                                                                                                                        (self :> IActor).DX <- (self :> IActor).DX * -1.0f
+                                                                                                                                                                            if (self :> IActor).DX < 0.0f then 
+                                                                                                                                                                                                        (self :> IActor).X <- (float32)j * 32.0f + 32.0f
+                                                                                                                                                                                                        (self :> IActor).DX <- (self :> IActor).DX * -1.0f
+
                                                                                                                                            if Core.tileMap.[i].[j] = 'T' then 
                                                                                                                                                 if(self :> IActor).DY > 0.0f then 
                                                                                                                                                                              if (self :> IActor).Hp > 0.0f then (self :> IActor).Hp <- (self :> IActor).Hp - 0.1f
                                                                                                                                                                              if (self :> IActor).Hp < 0.0f then (self :> IActor).IsLive <- false
             member self.move(dir) = 
-                               if dir = 1 then (self :> IActor).X <-  (self :> IActor).X + (self :>IActor).DX + 1.5f 
-                               if dir = 0 then (self :> IActor).X <-  (self :> IActor).X + (self :>IActor).DX - 1.5f 
-                                
+                               if dir = 1 
+                               && Core.tileMap.[(int)(((self :> IActor).Y) / 32.0f)].[(int)(((self :> IActor).X) / 32.0f)] <> '#' 
+                               then (self :> IActor).X <-  (self :> IActor).X + (self :>IActor).DX + 1.5f 
+                                    
+                               
+                               if dir = 0 
+                               && Core.tileMap.[(int)((self :> IActor).Y / 32.0f)].[(int)(((self :> IActor).X - 1.5f) / 32.0f)] <> '#' 
+                               then (self :> IActor).X <-  (self :> IActor).X + (self :>IActor).DX - 1.5f
+                           
+                             
+
+           
             member self.Update(time:float32) = 
                                   (self :> IActor).X <- (self :> IActor).X + (self :> IActor).DX * time
-                                 
-                                  self.Collision(0)
-                                 
-                                  if not self.slimeOnGround then (self :> IActor).DY <- (self :> IActor).DY + 0.6f
-                                  (self :> IActor).Y <- (self :> IActor).DY + (self :> IActor).Y 
-                                  self.slimeOnGround <- false
+
+                                  self.Collision()
                                   
-                                  self.Collision(1)
+                                  if not self.slimeOnGround then (self :> IActor).DY <- (self :> IActor).DY + 0.6f
+                                  (self :> IActor).Y <-  (self :> IActor).Y + (self :> IActor).DY 
+                                  //self.slimeOnGround <- false
                                   
                                   if not (self :> IActor).IsLive then (self :> IActor).Sprite.Color <- SFML.Graphics.Color.Red
-                                   
+                                  //self.Collision(0)
                                   (self :> IActor).Sprite.Position <- new Vector2f((self :> IActor).X - offsetX, (self :> IActor).Y - offsetY)
-                                  (self :> IActor).DX <- 0.0f
+                                  //(self :> IActor).DX <- 0.0f
         
         //player
         type Player() =
@@ -102,7 +105,7 @@ open System
                                     let mutable hp = (int)(self :> IActor).Hp
                                     let mutable maxhp = (int)(self :> IActor).MaxHp
                                     let hps:string = "Lvl: "+self.Level.ToString()+"\nHp: "+hp.ToString()+" / "+maxhp.ToString()
-                                    hps+"\n\n"
+                                    hps+"\n"
 
             member self.Collision(dir:int) = 
                                               for i = (int)((self :> IActor).Y / 32.0f) to (int)(( (self :> IActor).Y + 30.0f) / 32.0f) do 
--- Program.fs
+++ Program.fs
@@ -16,92 +16,110 @@ let main argv =
 
     let player = new Actors.Player()
     let slime = new Actors.Slime()
+    let slimeOne = new Actors.Slime()
+    
     gameWin.SetVerticalSyncEnabled(true)
     gameWin.Closed.AddHandler(fun sender evArgs -> (sender :?> RenderWindow).Close())
 
     let rect = new RectangleShape()
-    
+    let bullet = new RectangleShape()
+
+    bullet.Size <- new Vector2f(16.0f, 16.0f)
+    bullet.FillColor <- SFML.Graphics.Color((byte)0, (byte)255, (byte)25)
+
     rect.Size <- new Vector2f(32.0f, 32.0f)
             
     let playercord = new Text("player X: "+(player :> IActor).X.ToString()+" player Y: "+(player :> IActor).Y.ToString(), Content.Font)
+    
     playercord.FillColor <- SFML.Graphics.Color.Red
     playercord.Position <- new Vector2f(150.0f, 0.0f)
     playercord.CharacterSize <- 5u
 
     let playerHealth = new Text("", Content.Font)
     playerHealth.CharacterSize <- 15u
+    
     playerHealth.FillColor <- SFML.Graphics.Color((byte)155, (byte)255, (byte)25)
     playerHealth.OutlineColor <- SFML.Graphics.Color.Black
+    
     playerHealth.OutlineThickness <- 2.0f
+    
     let statusString = new Text(player.StatusString, Content.Font)
+    
     statusString.FillColor <- SFML.Graphics.Color.Yellow
+    
     playercord.CharacterSize <- 15u
             
     let smilecord = new Text("HP: "+(slime :> IActor).Hp.ToString(), Content.Font)
     smilecord.Position  <- new Vector2f(200.0f, 50.0f)
     smilecord.CharacterSize <- 15u
+    smilecord.OutlineThickness <- 2.0f
+    
     let clock =  new Clock()
-    
-    let rec mainLoop() = 
-                       gameWin.DispatchEvents()
-                       gameWin.Clear()
-                       let mutable time = clock.ElapsedTime.AsMilliseconds()
-                       clock.Restart() |> ignore
-                       offsetX <- (player :> IActor).X - 600.0f / 2.0f
-                       offsetY <- (player:> IActor).Y - 800.0f / 2.0f
-                       gameWin.SetTitle("Level: "+level.ToString())
-                       
-                       //slime.move(1)
-                        
-                       slime.Update((float32)time)   
-                       smilecord.Position  <- new Vector2f((slime :> IActor).X - 16.0f - offsetX, (slime :> IActor).Y - 32.0f - offsetY)     
-                        
-                       
-                       
-                       //if Keyboard.IsKeyPressed(Keyboard.Key.LShift) then 
-                       if Keyboard.IsKeyPressed(Keyboard.Key.Left) then player.move(Keyboard.Key.Left)                                              
-                       if Keyboard.IsKeyPressed(Keyboard.Key.Up) then player.move(Keyboard.Key.Up)
-                       if Keyboard.IsKeyPressed(Keyboard.Key.Right) then player.move(Keyboard.Key.Right)
-                       if Keyboard.IsKeyPressed(Keyboard.Key.Down) then player.move(Keyboard.Key.Down)
-                       if Keyboard.IsKeyPressed(Keyboard.Key.Escape) then gameWin.Close()
-                       
-                       playerHealth.Position <- new Vector2f((player :> IActor).X - 16.0f - offsetX, (player :> IActor).Y - 40.0f - offsetY)
-                       playerHealth.DisplayedString <- player.StatusString
-                       
-                       if (player :> IActor).IsLive then
-                                                    player.Update((float32)time)
-                                                               
-                                 
-                      
-                                   
-                               
-                       playercord.DisplayedString <- "player X: "+(player :> IActor).X.ToString()+" player Y: "+(player :> IActor).Y.ToString()
-                       
-                       statusString.DisplayedString <- player.StatusString
-                                    //draw here!!!
-                       
-                       for i = 0 to H - 1  do
-                                           for j = 0 to W - 1  do 
-                                                            if tileMap.[i].[j] = '#' then rect.FillColor <- SFML.Graphics.Color.White
-                                                            if tileMap.[i].[j] = '-' then rect.FillColor <- SFML.Graphics.Color.Transparent
-                                                            if tileMap.[i].[j] = 'T' then rect.FillColor <- SFML.Graphics.Color.Red
-                                                            if tileMap.[i].[j] = 'N' then rect.FillColor <- SFML.Graphics.Color.Blue
-                                                            if tileMap.[i].[j] = 'P' then rect.FillColor <- SFML.Graphics.Color.Yellow
+    while gameWin.IsOpen do
+        
+        gameWin.DispatchEvents()
+        gameWin.Clear()
+        let mutable time = clock.ElapsedTime.AsMilliseconds()
+        clock.Restart() |> ignore
+        offsetX <- (player :> IActor).X - 600.0f / 2.0f
+        offsetY <- (player:> IActor).Y - 800.0f / 2.0f
+        gameWin.SetTitle("Level: "+level.ToString())
+        
+        //slime.move(1)
+         
+        slime.Update((float32)time)   
+        smilecord.Position  <- new Vector2f((slime :> IActor).X - offsetX, (slime :> IActor).Y - 32.0f - offsetY)     
+        smilecord.DisplayedString <- "HP: "+(slime :> IActor).Hp.ToString()+"\nx:"+((int)((slime :>IActor).X / 32.0f)).ToString() + " y:"+((int)((slime :> IActor).Y / 32.0f)).ToString()
+        slimeOne.Update((float32)time)
+        
+        //if Keyboard.IsKeyPressed(Keyboard.Key.LShift) then 
+        if Keyboard.IsKeyPressed(Keyboard.Key.Left) then player.move(Keyboard.Key.Left)                                              
+        if Keyboard.IsKeyPressed(Keyboard.Key.Up) then player.move(Keyboard.Key.Up)
+        
+        if Keyboard.IsKeyPressed(Keyboard.Key.Right) then player.move(Keyboard.Key.Right)
+        
+        if Keyboard.IsKeyPressed(Keyboard.Key.Down) then player.move(Keyboard.Key.Down)
+        if Keyboard.IsKeyPressed(Keyboard.Key.Escape) then gameWin.Close()
 
-                                                            rect.Position <- new Vector2f((float32)j * 32.0f - offsetX, (float32)i * 32.0f - offsetY)
-                                                            gameWin.Draw(rect)
+        playerHealth.Position <- new Vector2f((player :> IActor).X - offsetX, (player :> IActor).Y - 40.0f - offsetY)
+        playerHealth.DisplayedString <- player.StatusString+"x: "+((int)((player :>IActor).X / 32.0f)).ToString()+" y: "+((int)((player :> IActor).Y / 32.0f)).ToString()
+        
+        if (player :> IActor).IsLive then
+                                     player.Update((float32)time)
+                                                
+                  
+                      
+                    
+                
+        playercord.DisplayedString <- "player X: "+(player :> IActor).X.ToString()+" player Y: "+(player :> IActor).Y.ToString()
+        
+        statusString.DisplayedString <- player.StatusString
+                     //draw here!!!
+        
+        for i = 0 to H - 1  do
+                            for j = 0 to W - 1  do 
+                                             if tileMap.[i].[j] = '#' then rect.FillColor <- SFML.Graphics.Color.White
+                                             if tileMap.[i].[j] = '-' then rect.FillColor <- SFML.Graphics.Color.Transparent
+                                             if tileMap.[i].[j] = 'T' then rect.FillColor <- SFML.Graphics.Color.Red
+                                             if tileMap.[i].[j] = 'N' then rect.FillColor <- SFML.Graphics.Color.Blue
+                                             if tileMap.[i].[j] = 'P' then rect.FillColor <- SFML.Graphics.Color.Yellow
+                                             if tileMap.[i].[j] = 's' then
+                                                                         (slimeOne :> IActor).Sprite.Position <- new Vector2f((float32)j * 32.0f - offsetX, (float32)i * 32.0f - offsetY)
+                                                                         (slimeOne :> IActor).Sprite.Color <- SFML.Graphics.Color.Red
+                                                                         tileMap.[i] <- tileMap.[i].Replace(tileMap.[i].[j], '-')
+                                                                         
+
+                                             rect.Position <- new Vector2f((float32)j * 32.0f - offsetX, (float32)i * 32.0f - offsetY)
+                                             gameWin.Draw(rect)
     
-                       if not (player :> IActor).IsLive then MessageBox.Show("You Dead!", gameWin)
-                       
-                       gameWin.Draw((player :> IActor).Sprite)
-                       gameWin.Draw(smilecord)
-                       gameWin.Draw((slime :> IActor).Sprite)
-                       gameWin.Draw(playerHealth)
-                       gameWin.Display()
-                        
-                       
-                       match gameWin.IsOpen with 
-                       | true -> mainLoop() |> ignore
-                       | false -> ()
-    mainLoop()
+        if not (player :> IActor).IsLive then MessageBox.Show("You Dead!", gameWin)
+        
+        gameWin.Draw((player :> IActor).Sprite)
+        gameWin.Draw(smilecord)
+        gameWin.Draw((slimeOne :> IActor).Sprite)
+        gameWin.Draw((slime :> IActor).Sprite)
+        gameWin.Draw(playerHealth)
+        
+        gameWin.Display()
+         
     0 // return an integer exit code
